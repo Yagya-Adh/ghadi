570358a792f5e5f66f4e4d8347e9ed662409ba53
Yagya-Adh <yagyaadhikari02@gmail.com>
2023-07-19 23:32:31 +0545

    add date

--- Test/Test.php
+++ Test/Test.php
@@ -5,3 +5,5 @@ require_once __DIR__ . '../../vendor/autoload.php';
 use Yagya\Ghadi\GhadiProvider\Ghadi;
 
 $obj = new Ghadi();
+
+$obj->show();
--- src/GhadiProvider/Ghadi.php
+++ src/GhadiProvider/Ghadi.php
@@ -2,12 +2,21 @@
 
 namespace Yagya\Ghadi\GhadiProvider;
 
+use DateTime;
 
 class Ghadi
 {
 
+    public $time;
+
     public function __construct()
     {
         echo 'The new Ghadi is lunch.';
+    }
+
+
+    public function show()
+    {
+        echo  $this->time = date("l jS \of F Y h:i:s A");
     }
 }
